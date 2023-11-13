e72613d714d80bac12f4877b14d285c836493858
Mortelle Underwood Jr <100214386+mujr93@users.noreply.github.com>
2023-11-13 09:32:33 -0600

    Update ParallelUtils.groovy

--- src/org/test/lib/ParallelUtils.groovy
+++ src/org/test/lib/ParallelUtils.groovy
@@ -10,17 +10,18 @@ class ParallelUtils {
         this.script = script
     }
 
-    @NonCPS
     def runPara() {
         def environments = ["development", "staging", "production", "testing", "stress", "pre-production", "post-production", "qa"]
-
+        def threads = []
+        
         for (def i = 0; i < environments.size(); i++) {
             def kobe = environments[i]
-            def t = new Thread({
+            threads << Thread.start {
                 sleep(10000)
-                steps.echo kobe
-            })
-            t.start()
+                println kobe
+            }
         }
+        
+        threads*.join()
     }
 }
