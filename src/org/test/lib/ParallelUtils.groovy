ba94b05b4e254209be96b6293ecfa88ba4b4e0c5
Mortelle Underwood Jr <100214386+mujr93@users.noreply.github.com>
2023-11-13 12:28:14 -0600

    Update ParallelUtils.groovy

--- src/org/test/lib/ParallelUtils.groovy
+++ src/org/test/lib/ParallelUtils.groovy
@@ -1,29 +1,10 @@
-package org.test.lib
+def environments = ["development", "staging", "production", "testing", "stress", "pre-production", "post-production", "qa"]
+def threads = []
 
-class ParallelUtils {
-    
-    def steps
-    def script
-
-    def ParallelUtils(steps, script) {
-        this.steps = steps
-        this.script = script
-    }
-
-    def runPara() {
-        def environments = ["development", "staging", "production", "testing", "stress", "pre-production", "post-production", "qa"]
-        def threads = []
-        
-        for (def i = 0; i < environments.size(); i++) {
-            def kobe = environments[i]
-            threads[i] = Thread.start {
-                sleep(10000)
-                println kobe
-            }
-        }
-
-        for (def i = 0; i < threads.size(); i++) {
-            threads[i].join()
-        }
+for (def i = 0; i < environments.size(); i++) {
+    def kobe = environments[i]
+    threads[i] = Thread.start {
+        sleep(10000)
+        println kobe
     }
 }
