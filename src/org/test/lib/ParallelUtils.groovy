25f9bdc14cd1b44e7354204c6ebf0aea9d2f7929
Mortelle Underwood Jr <100214386+mujr93@users.noreply.github.com>
2023-11-13 12:28:45 -0600

    Update ParallelUtils.groovy

--- src/org/test/lib/ParallelUtils.groovy
+++ src/org/test/lib/ParallelUtils.groovy
@@ -1,9 +1,8 @@
 def environments = ["development", "staging", "production", "testing", "stress", "pre-production", "post-production", "qa"]
-def threads = []
 
 for (def i = 0; i < environments.size(); i++) {
     def kobe = environments[i]
-    threads[i] = Thread.start {
+    Thread.start {
         sleep(10000)
         println kobe
     }
