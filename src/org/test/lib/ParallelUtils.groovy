70b9d3de5ccf71694008081bf43f4ab6f0229df4
Mortelle Underwood Jr <100214386+mujr93@users.noreply.github.com>
2023-11-12 23:11:51 -0600

    Update ParallelUtils.groovy

--- src/org/test/lib/ParallelUtils.groovy
+++ src/org/test/lib/ParallelUtils.groovy
@@ -1,5 +1,7 @@
 package org.test.lib
 
+import static groovyx.gpars.GParsPool.*
+
 class ParallelUtils {
     
     def steps
@@ -11,10 +13,26 @@ class ParallelUtils {
     }
     
     def runPara() {
-    def testSteps = ['setupDevkit', 'executeDevkit', 'restoreDevkit']
+        def mylist = ['item1','item2']
+        withPool {
+            mylist.eachParallel {
+                fun1(it)
+                fun2(it)
+            }
+        }
+        //def testSteps = ['setupDevkit', 'executeDevkit', 'restoreDevkit']
+    
+        //testSteps.stream().filter(s -> s != 'setupDevkit').forEach(s -> steps.echo s)
+        //testSteps.stream().forEach(s -> steps.echo s)
+        //testSteps.parallelStream().forEach(s -> steps.echo s)
+    }
 
-    testSteps.stream().filter(s -> s != 'setupDevkit').forEach(s -> steps.echo s)
-    testSteps.stream().forEach(s -> steps.echo s)
-    testSteps.parallelStream().forEach(s -> steps.echo s)
+    def fun1(s) {
+        println "function-1 - ${s}"
+    }
+    
+    def fun2(s) {
+        println "function-2 - ${s}"
+    }
   }
 }
