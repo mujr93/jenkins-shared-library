7181d78c3e0b250b934f3aeac64901b1759208ae
Mortelle Underwood Jr <100214386+mujr93@users.noreply.github.com>
2023-11-13 00:24:40 -0600

    Update ParallelUtils.groovy

--- src/org/test/lib/ParallelUtils.groovy
+++ src/org/test/lib/ParallelUtils.groovy
@@ -13,25 +13,14 @@ class ParallelUtils {
     }
     
     def runPara() {
-        def mylist = ['item1','item2']
-        withPool {
-            mylist.eachParallel {
-                fun1(it)
-                fun2(it)
+        def environments = ["development", "staging", "production", "testing", "stress", "pre-production", "post-production", "qa"]
+
+        for (def i = 0; i < environments.size(); i++) {
+            def kobe = environments[i]
+            Thread.start {
+                sleep(10000)
+                steps.echo kobe
             }
         }
-        //def testSteps = ['setupDevkit', 'executeDevkit', 'restoreDevkit']
-    
-        //testSteps.stream().filter(s -> s != 'setupDevkit').forEach(s -> steps.echo s)
-        //testSteps.stream().forEach(s -> steps.echo s)
-        //testSteps.parallelStream().forEach(s -> steps.echo s)
-    }
-
-    def fun1(s) {
-        println "function-1 - ${s}"
-    }
-    
-    def fun2(s) {
-        println "function-2 - ${s}"
     }
 }
