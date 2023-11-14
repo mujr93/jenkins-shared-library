6f90fa3491255b28ecdda63787721d659177eaa0
Mortelle Underwood Jr <100214386+mujr93@users.noreply.github.com>
2023-11-13 18:56:44 -0600

    Update para.groovy

--- vars/para.groovy
+++ vars/para.groovy
@@ -1,5 +1,5 @@
 def call() {
   withGroovy {
-    sh "groovy src/org/test/lib/ParallelUtils.groovy [1, 2, 3, 4] foo bar"
+    sh "groovy src/org/test/lib/ParallelUtils.groovy 1,2,3,4 foo bar"
   }
 }
