0742303b5cd15b8618f20a57c842444a46f31305
Mortelle Underwood Jr <100214386+mujr93@users.noreply.github.com>
2023-11-13 22:40:47 -0600

    test

--- src/org/test/lib/ParallelUtils.groovy
+++ src/org/test/lib/ParallelUtils.groovy
@@ -13,7 +13,7 @@ class ParallelUtils {
     def runPara(environments) {
         def s = """
             def proc = 'groovy -v'.execute()
-            println "${proc.in.text ?: proc.err.text}"
+            println proc.in.text ?: proc.err.text
             def environments = ['kobe', 'lebron', 'cp3']
             for (def i = 0; i < environments.size(); i++) {
                 def environment = environments[i]
