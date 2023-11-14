a42d5a05b7043d2c4eb1d3e7c4fc963bc572a0e3
Mortelle Underwood Jr <100214386+mujr93@users.noreply.github.com>
2023-11-13 18:34:32 -0600

    Update ParallelUtils.groovy

--- src/org/test/lib/ParallelUtils.groovy
+++ src/org/test/lib/ParallelUtils.groovy
@@ -1,14 +1,19 @@
-def environments = ["development", "staging", "production", "testing", "stress", "pre-production", "post-production", "qa"]
+class SomeClass {
 
-for (def i = 0; i < environments.size(); i++) {
-    def environment = environments[i]
-    Thread.start {
-        println environment
+    static void main(args) {
+        def environments = ["development", "staging", "production", "testing", "stress", "pre-production", "post-production", "qa"]
+
+        for (def i = 0; i < environments.size(); i++) {
+            def environment = environments[i]
+            Thread.start {
+                println environment
+            }
+        }
+        
+        println args.size()
+        
+        for (def i = 0; i < args.size(); i++) {
+            println args[i]
+        }
     }
-}
-
-println args.size()
-
-for (def i = 0; i < args.size(); i++) {
-    println args[i]
 }
