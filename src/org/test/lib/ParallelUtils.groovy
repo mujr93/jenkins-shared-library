2d30997485faa6c6fbdfeac14884422241735238
Mortelle Underwood <Mortelle.Underwood@sony.com>
2023-11-14 10:27:46 -0600

    Test

--- src/org/test/lib/ParallelUtils.groovy
+++ src/org/test/lib/ParallelUtils.groovy
@@ -1,30 +1,3 @@
-package org.test.lib
-
-class ParallelUtils {
-
-    def steps
-    def script
-
-    def ParallelUtils(steps, script) {
-        this.steps = steps
-        this.script = script
-    }
-
-    def runPara(environments) {
-        steps.withGroovy {
-            steps.sh "groovy -e 'new ParallelUtils(steps, script).testPara()'"
-        }
-    }
-
-    def testPara() {
-        def proc = 'groovy -v'.execute()
-        println proc.in.text ?: proc.err.text
-        def environments = ['kobe', 'lebron', 'cp3']
-        for (def i = 0; i < environments.size(); i++) {
-            def environment = environments[i]
-            Thread.start {
-                println environment
-            }
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2430de24a77879fcc903bc68309e11c161190dab2360af3ab6fde562a2457e3b
+size 720
