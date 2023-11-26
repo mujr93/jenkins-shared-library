e885c1b76b515ca83d63432143162da7c289645d
Mortelle Underwood <Mortelle.Underwood@sony.com>
2023-11-26 00:42:52 -0600

    Test

--- src/org/test/lib/ParallelUtils.groovy
+++ src/org/test/lib/ParallelUtils.groovy
@@ -1,5 +1,7 @@
 package org.test.lib
 
+import java.util.concurrent.*
+
 class ParallelUtils {
 
     def script
@@ -9,8 +11,20 @@ class ParallelUtils {
     }
 
     def runPara(environments) {
-        environments.stream()
-            .parallel()
-            .forEach { script.log.info("${it}") }
+        ExecutorService executorService = Executors.newFixedThreadPool(environments.size())
+        List<CompletableFuture<Void>> futures = environments
+        for (int i = 0; i < environments.size(); i++) {
+            def environment = environments[i]
+            CompletableFuture<Void> future = CompletableFuture.runAsync({
+                try {
+                    script.echo environment
+                } catch (InterruptedException e) {
+                    e.printStackTrace()
+                }
+            }, executorService)
+            futures.add(future)
+        }
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join()
+        executorService.shutdown()
     }
 }
