d5ad985cd96e63d119c295463bfbcf8283f4606d
Mortelle Underwood <Mortelle.Underwood@sony.com>
2023-11-26 19:36:16 -0600

    Testing scripts

---
+++ scripts/Threads.groovy
@@ -0,0 +1,14 @@
+def environments = ['development', 'staging', 'production', 'testing', 'stress', 'pre-production', 'post-production', 'qa']
+def threads = []
+
+for (int i = 0; i < environments.size(); i++) {
+  def environment = environments[i]
+  threads.add(Thread.start {
+    println "During: ${Thread.currentThread().getName()} - ${environment}"
+  })
+}
+
+for (int i = 0; i < threads.size(); i++) {
+  def thread = threads[i]
+  thread.join()
+}
--- vars/para.groovy
+++ vars/para.groovy
@@ -2,5 +2,5 @@ import org.test.lib.ParallelUtils
 
 def call() {
   def parallelUtils = new ParallelUtils(this)
-  parallelUtils.runPara(["development", "staging", "production", "testing", "stress", "pre-production", "post-production", "qa"])
+  parallelUtils.runPara(['development', 'staging', 'production', 'testing', 'stress', 'pre-production', 'post-production', 'qa'])
 }
