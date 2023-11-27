da0d4535e86e7d5eb9b78ae766354621963e7309
Mortelle Underwood <Mortelle.Underwood@sony.com>
2023-11-26 20:51:01 -0600

    Testing scripts

--- scripts/Threads.groovy
+++ scripts/Threads.groovy
@@ -1,5 +1,3 @@
-import org.test.lib.ParallelUtils
-
 def environments = ['development', 'staging', 'production', 'testing', 'stress', 'pre-production', 'post-production', 'qa']
 def threads = []
 
@@ -15,6 +13,3 @@ for (int i = 0; i < threads.size(); i++) {
   def thread = threads[i]
   thread.join()
 }
-
-def parallelUtils = new ParallelUtils(this)
-parallelUtils(environments)
